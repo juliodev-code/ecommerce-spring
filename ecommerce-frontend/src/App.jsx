003467979d35aac808d477c899381098956f19a7
Julio Jaramillo <juliocesar0293@gmail.com>
2026-06-24 13:06:35 -0600

    Prepare React Project

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { FaBeer } from 'react-icons/fa'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 class="flex items-center justify-center  bg-gray-800 text-white text-2xl font-bold">
+        Hello world! <FaBeer />
+      </h1>
+    </>
+  )
+}
+
+export default App
